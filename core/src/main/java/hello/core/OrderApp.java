2ca2561e679e8b33564f9bc8aab3f1628e28c7d2
Kim, Sunho <63573287+kimsunho940904@users.noreply.github.com>
2021-10-31 18:47:50 +0900

    spring container 적용

--- core/src/main/java/hello/core/OrderApp.java
+++ core/src/main/java/hello/core/OrderApp.java
@@ -7,21 +7,30 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class OrderApp {
 
     public static void main(String[] args) {
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
 
-        AppConfig appConfig = new AppConfig();
-
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        //ApplicationContext = 스프링 컨테이너
+        //스프링 컨테이너는 @Configuration이 붙은 AppConfig를 설정 정보로 사용.
+        //@Bean 이라 적힌 메서드를 모두 호출해서 반환된 객체를 스프링 컨테이너에 등록.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(1L, "memberA", Grade.VIP);
 
         memberService.join(member);
-        Order order = orderService.createOrder(memberId, "itemA", 20000);
+        Order order = orderService.createOrder(memberId, "itemA", 15000);
         System.out.println("order = " + order.toString());
 
 
