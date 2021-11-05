6fd471e36cb0f48577fa854cda086fae36310182
Kim, Sunho <63573287+kimsunho940904@users.noreply.github.com>
2021-11-05 13:23:12 +0900

    비즈니스 로직 생성

---
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,42 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemberRepositoryImpl;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemberRepositoryImpl();
+
+
+    //회원 가입
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복 회원 검증
+
+        memberRepository.save(member);
+
+        return member.getId();
+
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //같은 이름이 있는지 중복 확인
+        memberRepository.findByName(member.getName())
+                // isPresent : null이 아니라, 어떤 값이 있으면.
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    //전체 회원 조회 기능
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
