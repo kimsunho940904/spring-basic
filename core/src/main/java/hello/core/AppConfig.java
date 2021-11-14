d28cb8a5ccc308f239748498780dfaa59a6afe82
Kim, Sunho <63573287+kimsunho940904@users.noreply.github.com>
2021-11-14 20:29:01 +0900

    AppConfig 리팩토링

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,10 +12,19 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository()); // 임플객체를 생성하면 메모리멤버리파지토리를 쓸꺼야.
+        return new MemberServiceImpl(memberRepository()); // 임플객체를 생성하면 메모리멤버리파지토리를 쓸꺼야.
+    }
+
+    //중복되니까 이렇게 하나로 빼주자!!
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
