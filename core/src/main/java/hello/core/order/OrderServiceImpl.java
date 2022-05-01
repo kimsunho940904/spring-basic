93aad6b6e189e287d562e247f67283d330a25a5d
Kim, Sunho <63573287+kimsunho940904@users.noreply.github.com>
2022-05-01 22:06:18 +0900

    수정

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,14 +3,11 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    //멤버저장소에서 멤버를 찾아야함.
     private final MemberRepository memberRepository;
-    //할인정책으로.
-    private final DiscountPolicy discountPolicy; //인터페이스에만 의존화.
+    private final DiscountPolicy discountPolicy;
 
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
